f8b62d5f63450c89b78aeaafb5b977b764e1a193
niyakopcheva <niyakopcheva@gmail.com>
2026-03-05 14:59:41 +0200

    Fixed input validation + code cleanup and change of initial values

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import ResultTable from "./components/ResultTable";
 
 function App() {
   const [userValues, setUserValues] = useState({
-    initialInvestment: null,
-    annualInvestment: null,
-    expectedReturn: null,
-    duration: null
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
   });
+
+  const inputIsValid = userValues.duration >= 1 && userValues.initialInvestment && userValues.annualInvestment && userValues.expectedReturn;
 
   return (
     <>
       <UserInput userValues={userValues} setUserValues={setUserValues} />
-      {(userValues.initialInvestment && userValues.annualInvestment && userValues.expectedReturn && userValues.duration)
-        && <ResultTable userData={userValues} />}
+      {!inputIsValid && <p className="center">Please enter duration greater than zero!</p>}
+      {inputIsValid && <ResultTable userData={userValues} />}
     </>
   )
 }
